0e62265314f5258925e4da69554f8345690c9b81
Yasasi-sandupama <yasasiugc@gmail.com>
2026-01-03 23:09:19 +0530

    add: task for menu functions

--- todo.py
+++ todo.py
@@ -1,3 +1,10 @@
 def show_menu():
     print("\n"+"="*30)
     print("MY TO DO LIST")
+    print("="*30)
+    print("1. View all tasks")
+    print("2.add new task")
+    print("3.make if the task is completed")
+    print("4.delete a task")
+    print("5.exit")
+    
